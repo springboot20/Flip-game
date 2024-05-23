9183dfdc1c9c062c9ff73bd467d3473cc5a85f9d
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2024-05-23 09:23:00 +0100

    files deleted and game board layout modified

--- main.js
+++ main.js
@@ -1,57 +1,58 @@
-const cards = document.querySelectorAll('.card')
-var isFlipped = false
-var firstCard;
-var secondCard;
+const cards = document.querySelectorAll('.card');
+let isFlipped = false;
+let firstCard;
+let secondCard;
 
-cards.forEach((card) => card.addEventListener('click', flipCard))
+let count = 0;
+
+cards.forEach((card) => card.addEventListener('click', flipCard));
 
 function flipCard() {
-     this.classList.add('flip')
-     if (!isFlipped) {
-          isFlipped = true
-          firstCard = this
-     } else {
-          secondCard = this
-          checkcard()
-          console.log(firstCard);
-     }
+  this.classList.add('flip');
+  if (!isFlipped) {
+    isFlipped = true;
+    firstCard = this;
+  } else {
+    secondCard = this;
+    checkCard();
+    console.log(firstCard);
+  }
 }
 
-function checkcard() {
-     if (firstCard.dataset.image === secondCard.dataset.image) {
-          success()
-     } else {
-          failed()
-     }
+function checkCard() {
+  if (firstCard.dataset.image === secondCard.dataset.image) {
+    success();
+  } else {
+    failed();
+  }
 }
-
 
 function success() {
-     firstCard.removeEventListener('click', flipCard)
-     secondCard.removeEventListener('click', flipCard)
-     reset()
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+  reset();
 }
 function failed() {
-     setTimeout(() => {
-          firstCard.classList.remove('flip')
-          secondCard.classList.remove('flip')
-          reset()
-     }, 1000);
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    reset();
+  }, 400);
 }
 
-function reset() {
-     isFlipped = false
-     firstCard = null
-     secondCard = null
-}
+(function shuffle() {
+  cards.forEach((card) => {
+    let index = Math.floor(Math.random() * 16);
+    card.style.order = index;
+  });
+})();
 
+console.log(document.querySelector('.resetBtn'));
 
-(function shufffle() {
-     cards.forEach((card) => {
-          let index = Math.floor(Math.random() * 16)
-          card.style.order = index
-     })
-})()
-
-
-document.querySelector('.resetBtn').addEventListener('click', reset)
+document.getElementById('btn').addEventListener('click', () => {
+  document.addEventListener('load', () => {
+    isFlipped = false;
+    firstCard = null;
+    secondCard = null;
+  });
+});
